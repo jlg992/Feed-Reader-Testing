20309e10f43715044d5aa5fce5d60d69d4c6dd3b
Jeff <jeffery.lemar.graham@gmail.com>
2018-04-11 21:46:18 -0400

    remove unnecessary done callback

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,102 +8,101 @@
 * to ensure they don't run until the DOM is ready.
 */
 $(function() {
-/* This is our first test suite - a test suite just contains
- * a related set of tests. This suite is all about the RSS
- * feeds definitions, the allFeeds variable in our application.
- */
-describe('RSS Feeds', function() {
-    /* This is our first test - it tests to make sure that the
-     * allFeeds variable has been defined and that it is not
-     * empty. Experiment with this before you get started on
-     * the rest of this project. What happens when you change
-     * allFeeds in app.js to be an empty array and refresh the
-     * page?
+    /* This is our first test suite - a test suite just contains
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
      */
-    it('are defined', function() {
-        expect(allFeeds).toBeDefined();
-        expect(allFeeds.length).not.toBe(0);
-    });
-
-
-    it('have urls', function() {
-        allFeeds.forEach(function(element) {
-            expect(element.url).toBeDefined();
-            expect(element.url).not.toBe('');
+    describe('RSS Feeds', function() {
+        /* This is our first test - it tests to make sure that the
+         * allFeeds variable has been defined and that it is not
+         * empty. Experiment with this before you get started on
+         * the rest of this project. What happens when you change
+         * allFeeds in app.js to be an empty array and refresh the
+         * page?
+         */
+        it('are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
         });
-    });
 
 
-    it('have names', function() {
-        allFeeds.forEach(function(element) {
-            expect(element.name).toBeDefined();
-            expect(element.name).not.toBe('');
+        it('have urls', function() {
+            allFeeds.forEach(function(element) {
+                expect(element.url).toBeDefined();
+                expect(element.url).not.toBe('');
+            });
         });
-    });
-});
 
 
-describe('The menu', function() {
-    var menu,
-        body,
-        bodyClass;
-
-    beforeEach(function() {
-        body = document.body;
-        bodyClass = body.className;
-    });
-
-    it('starts hidden', function() {
-        expect($(body).hasClass('menu-hidden')).toBe(true);
-    });
-
-    it('toggles visibility', function() {
-        menu = document.querySelector(".menu-icon-link");
-        menu.click();
-        expect($(body).hasClass('menu-hidden')).toBe(false);
-        menu.click();
-        expect($(body).hasClass('menu-hidden')).toBe(true);
-    });
-});
-
-describe('Initial Entries', function() {
-    var entriesLength;
-
-    beforeEach(function(done) {
-        loadFeed(0, function() {
-            done();
-        });
-    });
-
-    it('should have at least one entry', function(done) {
-        entriesLength = $('.feed .entry').length;
-        expect(entriesLength).toBeGreaterThan(0);
-        done();
-    });
-});
-
-describe('New Feed Selection', function() {
-    var feed1,
-        feed2;
-
-    beforeEach(function(done) {
-        loadFeed(0, function() {
-            feed1 = $('.feed').html();
-            loadFeed(1, function() {
-                feed2 = $('.feed').html();
-                done()
+        it('have names', function() {
+            allFeeds.forEach(function(element) {
+                expect(element.name).toBeDefined();
+                expect(element.name).not.toBe('');
             });
         });
     });
 
-    afterEach(function() {
-      loadFeed(0);
+
+    describe('The menu', function() {
+        var menu,
+            body,
+            bodyClass;
+
+        beforeEach(function() {
+            body = document.body;
+            bodyClass = body.className;
+        });
+
+        it('starts hidden', function() {
+            expect($(body).hasClass('menu-hidden')).toBe(true);
+        });
+
+        it('toggles visibility', function() {
+            menu = document.querySelector(".menu-icon-link");
+            menu.click();
+            expect($(body).hasClass('menu-hidden')).toBe(false);
+            menu.click();
+            expect($(body).hasClass('menu-hidden')).toBe(true);
+        });
     });
 
+    describe('Initial Entries', function() {
+        var entriesLength;
 
-    it('causes a content change', function(done) {
-        expect(feed1).not.toEqual(feed2);
-        done();
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('should have at least one entry', function(done) {
+            entriesLength = $('.feed .entry').length;
+            expect(entriesLength).toBeGreaterThan(0);
+            done();
+        });
     });
-});
+
+    describe('New Feed Selection', function() {
+        var feed1,
+            feed2;
+
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                feed1 = $('.feed').html();
+                loadFeed(1, function() {
+                    feed2 = $('.feed').html();
+                    done()
+                });
+            });
+        });
+
+        afterEach(function() {
+          loadFeed(0);
+        });
+
+
+        it('causes a content change', function() {
+            expect(feed1).not.toEqual(feed2);
+        });
+    });
 }());
